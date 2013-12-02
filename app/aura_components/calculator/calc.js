928ff2d65cca60a2d6e391eaae2d4ea3f5358dfa
Lyubomyr Rudko <lrudko@lohika.com>
2013-12-02 10:04:41 +0200

    Keyboard interaction done

--- app/aura_components/calculator/calc.js
+++ app/aura_components/calculator/calc.js
@@ -2,110 +2,116 @@ define([
     'underscore',
     'jquery'
 ], function (_, $) {
-    var params = [],
-        action = null,
-        maxInputDigits = 15,
-        actions = {
-            equal: 'equal',
-            ce: 'ce', //clear
-            add: 'add',
-            substruct: 'substruct',
-            multiply: 'multiply',
-            divide: 'divide'
-        };
+    function getInstance() {
+        var params = [],
+            action = null,
+            maxInputDigits = 15,
+            actions = {
+                equal: 'equal',
+                ce: 'ce', //clear
+                add: 'add',
+                substruct: 'substruct',
+                multiply: 'multiply',
+                divide: 'divide'
+            };
 
-    function addValue (val) {
-        var currentParam;
+        function addValue (val) {
+            var currentParam;
 
-        if (!params.length) {
-            params.push([]);
-        }
-
-        currentParam = params[params.length - 1];
-
-        if (currentParam instanceof Array) {
-            if (currentParam.length <= maxInputDigits) {
-                currentParam.push(val);
-            }
-        } else {
-            if (val === '.') {
-                val = '0.';
+            if (!params.length) {
+                params.push([]);
             }
 
-            if (action) {
-                params.push([val]);
+            currentParam = params[params.length - 1];
+
+            if (currentParam instanceof Array) {
+                if (currentParam.length <= maxInputDigits) {
+                    currentParam.push(val);
+                }
             } else {
-                params = [[val]];
+                if (val === '.') {
+                    val = '0.';
+                }
+
+                if (action) {
+                    params.push([val]);
+                } else {
+                    params = [[val]];
+                }
             }
         }
-    }
 
-    function addAction (a) {
-        if (a === actions.ce) {
-            params = [];
-            action = null;
-            return;
+        function addAction (a) {
+            if (a === actions.ce) {
+                params = [];
+                action = null;
+                return;
+            }
+
+            if (params.length === 2) {
+                performAction();
+            } else if (a !== actions.equal) {
+                params.push([]);
+            }
+
+            action = a;
+
+            if (action === actions.equal) {
+                action = null;
+            }
         }
 
-        if (params.length === 2) {
-            performAction();
-        } else if (action !== actions.equal) {
-            params.push([]);
+        function performAction () {
+            var value1,
+                value2 = parseFloat(params[1].join('')),
+                res = 0;
+
+            if (params[0] instanceof Array) {
+                value1 = parseFloat(params[0].join(''));
+            } else {
+                value1 = params[0];
+            }
+
+            if (action === actions.add) {
+                res = value1 + value2;
+            } else if (action === actions.substruct) {
+                res = value1 - value2;
+            } else if (action === actions.multiply) {
+                res = value1 * value2;
+            } else if (action === actions.divide) {
+                res = value1 / value2;
+            }
+
+            params = [res];
         }
 
-        action = a;
+        function getOutput () {
+            var value1,
+                value2;
 
-        if (action === actions.equal) {
-            action = null;
-        }
-    }
+            if (params[1]) {
+                value2 = parseFloat(params[1].join(''));
+            }
 
-    function performAction () {
-        var value1,
-            value2 = parseFloat(params[1].join('')),
-            res = 0;
+            if (params[0] instanceof Array) {
+                value1 = parseFloat(params[0].join(''));
+            } else {
+                value1 = params[0];
+            }
 
-        if (params[0] instanceof Array) {
-            value1 = parseFloat(params[0].join(''));
-        } else {
-            value1 = params[0];
-        }
-
-        if (action === actions.add) {
-            res = value1 + value2;
-        } else if (action === actions.substruct) {
-            res = value1 - value2;
-        } else if (action === actions.multiply) {
-            res = value1 * value2;
-        } else if (action === actions.divide) {
-            res = value1 / value2;
+            return value2 || value1 || '0';
         }
 
-        params = [res];
-    }
-
-    function getOutput () {
-        var value1,
-            value2;
-
-        if (params[1]) {
-            value2 = parseFloat(params[1].join(''));
-        }
-
-        if (params[0] instanceof Array) {
-            value1 = parseFloat(params[0].join(''));
-        } else {
-            value1 = params[0];
-        }
-
-        return value2 || value1 || '0';
+        return {
+            addValue: addValue,
+            addAction: addAction,
+            getOutput: getOutput,
+            actions: actions
+        };
     }
 
     return {
-        addValue: addValue,
-        addAction: addAction,
-        getOutput: getOutput,
-        actions: actions
+        getInstance: getInstance
     };
 
 });
--- app/aura_components/calculator/main.js
+++ app/aura_components/calculator/main.js
@@ -3,15 +3,16 @@ define([
     'module',
     'underscore',
     'jquery'
-], function(calc, module, _, $) {
-    var actionsMap = {
-        '+': calc.actions.add,
-        '-': calc.actions.substruct,
-        '*': calc.actions.multiply,
-        '/': calc.actions.divide,
-        '=': calc.actions.equal,
-        'ce': calc.actions.ce
-    };
+], function(calculator, module, _, $) {
+    var calc = calculator.getInstance(),
+        actionsMap = {
+            '+': calc.actions.add,
+            '-': calc.actions.substruct,
+            '*': calc.actions.multiply,
+            '/': calc.actions.divide,
+            '=': calc.actions.equal,
+            'ce': calc.actions.ce
+        };
 
     return {
         templates: 'tpl',
@@ -36,10 +37,43 @@ define([
         initialize: function() {
             this.render();
             this.sandbox.utils.loadCssForModule(module);
+            this.handleKeypress();
         },
 
         render: function(examsList) {
             this.html(this.renderTemplate('tpl'));
+        },
+
+        handleKeypress: function () {
+            var that = this;
+
+            $(document).keypress(function (e) {
+                var value,
+                    display = that.$el.find('span.display');
+
+                if (e.charCode) {
+                    value = String.fromCharCode(e.charCode);
+                    if (actionsMap[value]) {
+                        calc.addAction(actionsMap[value]);
+
+                        that.animateBtnClick(value);
+                    } else if (/^\d+$/.test(value)) {
+                        calc.addValue(value);
+                        that.animateBtnClick(value);
+
+                    }
+                }
+
+                display.html(calc.getOutput());
+            });
+
+            // $(document).keydown(cb);
+        },
+        animateBtnClick: function (value) {
+            this.$el.find('a[data-calculator-button="' + value + '"]').addClass("active").delay(100).queue(function(next){
+                $(this).removeClass("active");
+                next();
+            });
         }
     };
 });
